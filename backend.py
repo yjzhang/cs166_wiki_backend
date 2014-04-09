0c45730ba5018e8693e918fc6dc8f464ecace752
yjzhang <jack@patchouli.(none)>
2014-04-09 14:58:19 -0400

    better backend

--- backend.py
+++ backend.py
@@ -27,6 +27,7 @@ def save_browser_data():
     results = cursor.fetchall()
     results_str = ""
     if len(results) > 0:
+        results_str += 'Browser signature: you, or someone with the same browser hash as you, has visited this site before.\n\n'
         for result in results:
             old_ip = result[0]
             timestamp = result[1]
@@ -35,6 +36,21 @@ def save_browser_data():
             results_str += old_ip + '\t' + timestamp + '\t' + lat1 + '\t' + lon1 + '\n'
     else:
         cursor.execute('INSERT INTO hashes values (?)', (hash,))
+        results_str += "You have not visited this site using this browser before.\n"
+        # now, look up IP
+    results_str += '\n\n'
+    cursor.execute('SELECT ip, datetime, lat, lon FROM ip_hashes WHERE ip=?', (ip,))
+    results = cursor.fetchall()
+    if len(results) > 0:
+        results_str += 'IP Address: It seems that you, or someone with your IP address, has visited this site before.\n\n'
+        for result in results:
+            old_ip = result[0]
+            timestamp = result[1]
+            lat1 = result[2]
+            lon1 = result[3]
+            results_str += old_ip + '\t' + timestamp + '\t' + lat1 + '\t' + lon1 + '\n'
+    else:
+        results_str += 'Based on IP address, it seems that you have not visited this site before.\n'
     current_time = time.gmtime()
     time_str = time.strftime('%Y-%m-%d %H:%M:%S', current_time)
     cursor.execute('INSERT INTO ip_hashes values (?, ?, ?, ?, ?)', (ip, hash, time_str, lat, lon))
